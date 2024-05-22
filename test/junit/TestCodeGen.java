0d7a9d81098bfae762ab9f55c864ee84979fcbe0
Andy Stanciu <andreist@outlook.com>
2024-05-21 23:53:50 -0700

    Tests for codegen

--- test/junit/TestCodeGen.java
+++ test/junit/TestCodeGen.java
@@ -6,20 +6,25 @@ import org.junit.Test;
 
 public class TestCodeGen {
     public static final String TEST_FILES_LOCATION = "test/resources/CodeGen/";
-    public static final String TEST_FILES_INPUT_EXTENSION = ".java";
 
     private void runCompilerTest(String name) {
         try {
             new MiniJavaTestBuilder()
                     .testCompiledProgramOutputMatchesJava(
-                            Path.of(TEST_FILES_LOCATION, name + TEST_FILES_INPUT_EXTENSION));
+                            Path.of(TEST_FILES_LOCATION, name));
         } catch (IOException | InterruptedException e) {
             fail(e.getMessage());
         }
     }
 
     @Test
-    public void testFlow1() {
-//        runCompilerTest("Flow1");
+    public void testAll() {
+        var path = CSE401TestUtils._fixPath(Path.of(TEST_FILES_LOCATION));
+        var sourceDirectory = path.toFile();
+        for (var file : sourceDirectory.listFiles()) {
+            if (file.isFile()) {
+                runCompilerTest(file.getName());
+            }
+        }
     }
 }
