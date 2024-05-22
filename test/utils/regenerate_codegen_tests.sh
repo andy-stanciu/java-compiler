07a55e6da480dcb71700a16bec399745cfe0d05b
Andy Stanciu <andreist@outlook.com>
2024-05-21 23:28:55 -0700

    fix codegen regeneration

--- test/utils/regenerate_codegen_tests.sh
+++ test/utils/regenerate_codegen_tests.sh
@@ -2,8 +2,9 @@
 # this script regenerates tests for codegen. WARNING: do not use unless you're 100% sure
 # that you want to regenerate (and thus overwrite) all the existing codegen tests.
 cd ../..
-for file in ./test/resources/CodeGen/*.java; do
+for file in ./test/resources/Codegen/*.java; do
   echo "Compiling ${file%.*}.java..."
-  java -cp "build/classes;lib/*" MiniJava "$file" > "${file%.*}.S"
+  filename=$(basename "$file")
+  java -cp "build/classes;lib/*" MiniJava "$file" > "./test/resources/Codegen/out/${filename%.*}.S"
 done
 exit 0
