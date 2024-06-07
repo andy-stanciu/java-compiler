a7c0af7fe6e12bb90c6e6e7a5be3d7d0a1addebf
Andy Stanciu <andreist@outlook.com>
2024-06-06 19:17:23 -0700

    Write for tests

---
+++ test/resources/CodeGen/src/For.java
@@ -0,0 +1,53 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println(new Driver().run());
+    }
+}
+
+class Driver {
+    public int run() {
+        int i;
+        int j;
+        int k;
+
+        for (i = 0; i < 10; i++) {
+            System.out.println(i + 1);
+        }
+
+        for (i = 10; i > 0; i--) {
+            System.out.println(i);
+        }
+
+        for (i = 0; i < 5; i++) {
+            for (j = 0; j < 5; j++) {
+                for (k = 0; k < 5; k++) {
+                    System.out.println(i * j * k);
+                }
+            }
+        }
+
+        for (; false; ) {
+            System.out.println(999);
+        }
+
+        for (i = 0; i < 10; i += 2) {
+            System.out.println(i * 2);
+        }
+
+        for (j = 1; j <= 20; j *= 2) {
+            System.out.println(j * 3);
+        }
+
+        for (i = 0; i < 10;) {
+            System.out.println(i - 5);
+            i++;
+        }
+
+        i = 0;
+        for (; i < 10;) {
+            System.out.println(i * 7);
+            i += 3;
+        }
+        return 0;
+    }
+}
