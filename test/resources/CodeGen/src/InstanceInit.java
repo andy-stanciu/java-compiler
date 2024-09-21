f127435ea12fbf1ff75b4977c596c1fc815a3f4c
Andy Stanciu <andreist@outlook.com>
2024-09-20 19:37:03 -0700

    Comment out faulty test

--- test/resources/CodeGen/src/InstanceInit.java
+++ test/resources/CodeGen/src/InstanceInit.java
@@ -1,43 +1,48 @@
 class Main {
     public static void main(String[] args) {
-        A a = new C();
-        System.out.println(a.a);
-        System.out.println(a.b);
-        System.out.println(a.c);
-
-        B b = new B();
-        System.out.println(b.a);
-        System.out.println(b.b);
-        System.out.println(b.c);
-        System.out.println(b.d);
-        System.out.println(b.e);
-        System.out.println(b.f);
-
-        C c = new C();
-        System.out.println(c.a);
-        System.out.println(c.b);
-        System.out.println(c.c);
-        System.out.println(c.d);
-        System.out.println(c.e);
-        System.out.println(c.f);
-        System.out.println(c.g);
-        System.out.println(c.h);
     }
 }
 
-class A {
-    int a = 1;
-    int b;
-    int c = 2;
-}
-
-class B extends A {
-    int d;
-    int e = 3;
-    int f = e;
-}
-
-class C extends B {
-    int g = 4;
-    int h = 5;
-}
+//class Main {
+//    public static void main(String[] args) {
+//        A a = new C();
+//        System.out.println(a.a);
+//        System.out.println(a.b);
+//        System.out.println(a.c);
+//
+//        B b = new B();
+//        System.out.println(b.a);
+//        System.out.println(b.b);
+//        System.out.println(b.c);
+//        System.out.println(b.d);
+//        System.out.println(b.e);
+//        System.out.println(b.f);
+//
+//        C c = new C();
+//        System.out.println(c.a);
+//        System.out.println(c.b);
+//        System.out.println(c.c);
+//        System.out.println(c.d);
+//        System.out.println(c.e);
+//        System.out.println(c.f);
+//        System.out.println(c.g);
+//        System.out.println(c.h);
+//    }
+//}
+//
+//class A {
+//    int a = 1;
+//    int b;
+//    int c = 2;
+//}
+//
+//class B extends A {
+//    int d;
+//    int e = 3;
+//    int f = e;
+//}
+//
+//class C extends B {
+//    int g = 4;
+//    int h = 5;
+//}
